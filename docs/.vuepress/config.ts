edce04e575f366b6138212e9e35e632266483569
dead_summer <2941325451@qq.com>
2025-07-25 18:47:55 +0800

    fix: repair web favicon

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -23,7 +23,10 @@ export default defineUserConfig({
 
   head: [
     // 配置站点图标
-    ['link', { rel: 'icon', href: '/blog/favicon.ico' }],
+    ['link', { rel: 'icon', href: 'favicon.ico' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon-16x16.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' }],
   ],
 
   bundler: viteBundler(),
